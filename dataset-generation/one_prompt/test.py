77f91fe9407305a9e52c23c26b636a7df267c6fa
Prashant Joshi <joshiprashanthd121@gmail.com>
2024-10-28 22:17:26 +0530

    add new prompts for dataset generations
    OnePrompt is primary one

--- dataset-generation/one_prompt/test.py
+++ dataset-generation/one_prompt/test.py
@@ -3,8 +3,8 @@ import sys
 sys.path.append("../")
 
 from Dataset import read_json_files
-from MentalHealthCategory import MentalHealthCategory
-from groq_model import Llama3_1_70B_Versatile
+from OnePrompt import OnePrompt
+from groq_model import Llama3_1_8B_Instant, Llama3_1_70B_Versatile
 from pprint import pprint
 
 json_file_path = "../sample_abstracts.json"
@@ -14,7 +14,7 @@ publications = read_json_files([json_file_path])
 model = Llama3_1_70B_Versatile(
     api_key="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"
 )
-prompt = MentalHealthCategory(model)
+prompt = OnePrompt(model)
 
-response = prompt(publications)
+response = prompt(publications[:2])
 pprint(response)
